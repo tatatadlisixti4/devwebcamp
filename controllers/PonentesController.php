5a77e4ead593f8a10450d4aaadde80abd8d3fd23
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-24 19:00:23 -0300

    Primeros pasos con el formulario de registro de ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -11,4 +11,9 @@ class PonentesController {
             'titulo' => 'Ponentes / Conferencistas'
         ]);
     }
+    public static function crear(Router $router) {
+        $router->render('admin/ponentes/crear', [
+            'titulo' => 'Registrar Ponente'
+        ]);
+    }
 }
