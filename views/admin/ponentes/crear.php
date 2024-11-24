e4f4e6b663c3299d2dfdaf1f732a15f7f1243a60
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-24 19:15:54 -0300

    Primeros pasos con el formulario de registro de ponentes 2

--- views/admin/ponentes/crear.php
+++ views/admin/ponentes/crear.php
@@ -1,7 +1,20 @@
 <h2 class="dashboard__heading"><?php echo $titulo;?></h2>
-<div class="dashboard__contenedor-boton">
+<div class="dashboard__contenedor-boton"
     <a href="/admin/ponentes" class="dashboard__boton">
         <i class="fa-solid fa-circle-arrow-left"></i>
         Volver
     </a>
+</div>
+
+<div class="dashboard_formulario">
+    <?php include_once __DIR__ . '/../../templates/alertas.php';?>
+    <form method="POST" action="/admin/ponentes/crear" class="formulario" enctype="multipart/form-data">
+        <?php include_once __DIR__ . '/formulario.php';?>
+        <input
+            type="submit"
+            value="Registrar Ponente"
+            class="formulario__submit"
+        />
+    </form>
+    
 </div>
---
+++ views/admin/ponentes/formulario.php
@@ -0,0 +1,26 @@
+<fieldset>
+    <legend>Información Personal</legend>
+    <div class="formulario__campo">
+        <label for="nombre" class="formulario__label">Nombre</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="nombre"
+            name="nombre"
+            placeholder="Nombre Ponente"
+            value="<?php echo $ponente->nombre ?? '';?>"
+        />
+    </div>
+
+    <div class="formulario__campo">
+        <label for="apellido" class="formulario__label">Apellido</label>
+        <input
+            type="text"
+            class="formulario__input"
+            id="apellido"
+            name="apellido"
+            placeholder="Apellido Ponente"
+            value="<?php echo $ponente->apellido ?? '';?>"
+        />
+    </div>
+</fieldset>
