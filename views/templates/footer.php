b63fa7a35f4b7e4c18a75f0e030791346d6c3b74
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-23 23:13:16 -0300

    Creando el menú de redes sociales

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -11,22 +11,22 @@
         </div>
 
         <nav class="menu-redes">
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com">
                 <span class="menu-redes__ocultar">Facebook</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com">
                 <span class="menu-redes__ocultar">Twitter</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.youtube.com">
                 <span class="menu-redes__ocultar">YouTube</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com">
                 <span class="menu-redes__ocultar">Instagram</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.tiktok.com">
                 <span class="menu-redes__ocultar">Tiktok</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com">
                 <span class="menu-redes__ocultar">Github</span>
             </a>
         </nav>
