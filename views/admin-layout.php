2605f7f4d8a8ec1217076bd1c6f9c2ad02a7bdb2
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-24 18:19:21 -0300

    CSS al panel de administración

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -12,12 +12,14 @@
     </head>
     <body class="dashboard">
         <?php include_once __DIR__ .'/templates/admin-header.php'; ?>
+
         <div class="dashboard__grid">
             <?php include_once __DIR__ .'/templates/admin-sidebar.php'; ?>
             <main class="dashboard__contenido">
                 <?php echo $contenido; ?>
             </main>
         </div>
+
         <script src="/build/js/bundle.min.js" defer></script>
     </body>
 </html>
