317ee6f56d8b6dc6738bba55615e34c8ef1db461
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-24 19:41:27 -0300

    Aplicando CSS al formulario

--- views/admin/ponentes/crear.php
+++ views/admin/ponentes/crear.php
@@ -1,20 +1,22 @@
 <h2 class="dashboard__heading"><?php echo $titulo;?></h2>
-<div class="dashboard__contenedor-boton"
-    <a href="/admin/ponentes" class="dashboard__boton">
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/admin/ponentes">
         <i class="fa-solid fa-circle-arrow-left"></i>
         Volver
     </a>
 </div>
 
-<div class="dashboard_formulario">
+<div class="dashboard__formulario">
     <?php include_once __DIR__ . '/../../templates/alertas.php';?>
+
     <form method="POST" action="/admin/ponentes/crear" class="formulario" enctype="multipart/form-data">
         <?php include_once __DIR__ . '/formulario.php';?>
+
         <input
             type="submit"
             value="Registrar Ponente"
-            class="formulario__submit"
+            class="formulario__submit formulario__submit--registrar"
         />
     </form>
-    
+
 </div>
