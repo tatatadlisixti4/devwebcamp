59e19dacd6f6c68b1a049809b819199ed73857ef
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-25 02:03:01 -0300

    Listar y eliminar tags con js

--- src/js/tags.js
+++ src/js/tags.js
@@ -7,6 +7,9 @@
     const tagsInput = $('#tags_input');
 
     if(tagsInput) {
+        const tagsDiv = $('#tags');
+        const tagsInputHidden = $('[name="tags"]');
+
         let tags = [];
         // Escuchar los cambios en el input
         $$$(tagsInput, 'keypress', guardarTag);
@@ -18,8 +21,31 @@
                 e.preventDefault();
                 tags = [...tags, e.target.value.trim()];
                 tagsInput.value = '';
-                console.log(tags);
+                mostrarTags();
             }
+        }
+
+        function mostrarTags() {
+            tagsDiv.textContent = '';
+            tags.forEach(tag => {
+                const etiqueta = document.createElement('LI');
+                etiqueta.classList.add('formulario__tag');
+                etiqueta.textContent = tag;
+                etiqueta.ondblclick = eliminarTag;
+                tagsDiv.appendChild(etiqueta);
+            });
+            actualizarInputHidden();
+        }
+
+        function eliminarTag(e) {
+            e.target.remove();
+            tags = tags.filter(tag => tag !== e.target.textContent);
+            actualizarInputHidden();
+            console.log(tags);
+
+        }
+        function actualizarInputHidden() {
+            tagsInputHidden.value = tags.toString();
         }
     }
 })();
