5536e67235c6a64b9d3100e6cf4d8c23f6412bc6
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-25 12:28:15 -0300

    Añadiendo la tabla, modelo y validación del ponente

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 
+use Model\Ponente;
 use MVC\Router;
 
 class PonentesController {
@@ -13,9 +14,19 @@ class PonentesController {
     }
     public static function crear(Router $router) {
         $alertas = [];
+        $ponente = new Ponente;
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $ponente->sincronizar($_POST);
+            $alertas = $ponente->validar();
+        }
+
+
+
         $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponente',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'ponente' => $ponente
         ]);
     }
 }
