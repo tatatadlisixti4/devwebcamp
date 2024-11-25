7b3e3b57e6a796561823053688f2c5ba7989f3ac
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-25 18:00:12 -0300

    Finalizar la actualización de ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -34,6 +34,7 @@ class PonentesController {
                 $nombre_imagen = md5(uniqid(rand(), true));
                 $_POST['imagen'] = $nombre_imagen;
             }
+
             $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
             $ponente->sincronizar($_POST);
             // Validar
@@ -75,6 +76,37 @@ class PonentesController {
         }
         $ponente->imagen_actual = $ponente->imagen;
 
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if(!empty($_FILES['imagen']['tmp_name'])) {
+                $carpeta_imagenes = '../public/img/speakers';
+                // Crear la carpeta si no existe
+                if(!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0777, true);
+                }
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+                $_POST['imagen'] = $nombre_imagen;
+            } else {
+                $_POST['imagen'] = $ponente->imagen_actual;
+            }
+
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+            $ponente->sincronizar($_POST);
+            if(empty($alertas)) {
+                if(isset($nombre_imagen)) {
+                    $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+                    $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.webp');
+                }
+                $resultado = $ponente->guardar();
+                if($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
+
+        $alertas = $ponente->validar();
         $router->render('admin/ponentes/editar', [
             'titulo' => 'Actualizar Ponente',
             'alertas' => $alertas,
