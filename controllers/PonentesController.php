e4df4217e6417b0782ae5851d09bc3e2db622a4c
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-27 14:14:29 -0300

    Obtener la página actual

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -8,7 +8,11 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
     public static function index(Router $router) {
-        $pagina_actual = 1;
+        $pagina_actual = $_GET['page'];
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+        if(!$pagina_actual || $pagina_actual < 1) {
+            header('Location: /admin/ponentes?page=1');
+        }
         $registros_por_pagina = 10;
         $total = 10;
 
