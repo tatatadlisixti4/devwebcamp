8f37872a8e7973f3106a108d0d05afe56f8b957e
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-03 14:26:02 -0300

    Validar un nuevo evento

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -5,6 +5,7 @@ namespace Controllers;
 
 use Model\Categoria;
 use Model\Dia;
+use Model\Evento;
 use Model\Hora;
 use MVC\Router;
 
@@ -16,17 +17,32 @@ class EventosController {
     }
     public static function crear(Router $router) {
         $alertas = [];
+        $evento = new Evento();
 
         $categorias = Categoria::all('ASC');
         $dias = Dia::all('ASC');
         $horas = Hora::all('ASC');
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $evento = new Evento();
+            $evento->sincronizar($_POST);
+            $alertas = $evento->validar();
+
+            if(empty($alertas)) {
+                $resultado = $evento->guardar();
+                if($resultado) {
+                    header('Location: /admin/eventos');
+                }
+            }
+        }
 
         $router->render('admin/eventos/crear', [
             'titulo' => 'Registrar Evento',
             'alertas' => $alertas,
             'categorias' => $categorias,
             'dias' => $dias,
-            'horas' => $horas
+            'horas' => $horas,
+            'evento' => $evento
         ]);
     }
 }
--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -10,6 +10,7 @@
             id="nombre"
             name="nombre"
             placeholder="Nombre Evento"
+            value="<?php echo $evento->nombre ?? '';?>"
         />
     </div>
     <div class="formulario__campo">
@@ -20,19 +21,22 @@
             name="descripcion"
             placeholder="Descripción Evento"
             rows="8"
-        ></textarea>
+        ><?php echo $evento->descripcion ?? '';?></textarea>
     </div>
+
     <div class="formulario__campo">
-        <label for="descripcion" class="formulario__label">Categoría o Tipo de Evento</label>
+        <label for="categoria" class="formulario__label">Categoría o Tipo de Evento</label>
         <select
             class="formulario__select"
             id="categoria"
             name="categoria_id"
-
         >
-            <option value="" disabled selected>-- Seleccionar --</option>
-            <?php foreach($categorias as $categoria) {?>
-                <option value="<?php echo $categoria->id;?>">
+            <option value="">-- Seleccionar --</option>
+            <?php foreach($categorias as $categoria) { ?>
+                <option
+                    <?php echo ($evento->categoria_id === $categoria->id)? 'selected': '';?>
+                    value="<?php echo $categoria->id;?>"
+                >
                     <?php echo $categoria->nombre;?>
                 </option>
             <?php } ?>
@@ -84,12 +88,13 @@
     <div class="formulario__campo">
         <label for="disponibles" class="formulario__label">Lugares Disponibles</label>
         <input
-                type="number"
-                min="1"
-                class="formulario__input"
-                id="disponibles"
-                name="disponibles"
-                placeholder="Ej. 20"
+            type="number"
+            min="1"
+            class="formulario__input"
+            id="disponibles"
+            name="disponibles"
+            placeholder="Ej. 20"
+            value="<?php echo $evento->disponibles ?? '';?>"
         />
     </div>
 
